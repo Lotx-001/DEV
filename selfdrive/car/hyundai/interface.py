70f3a85e7427d9fd12d44de73a577a3ab2f0c32e
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2021-11-16 07:10:09 +0300

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -144,8 +144,8 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.7
     elif candidate in [CAR.K7, CAR.K7_HEV]:
       tire_stiffness_factor = 0.7
-      ret.mass = 1580. + STD_CARGO_KG
-      ret.wheelbase = 2.855
+      ret.mass = 1575. + STD_CARGO_KG
+      ret.wheelbase = 2.85
     elif candidate == CAR.SELTOS:
       ret.mass = 1310. + STD_CARGO_KG
       ret.wheelbase = 2.6
